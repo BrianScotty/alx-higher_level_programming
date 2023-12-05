907e9851ea8458cd90f7280afd0f0ea7d6ec0d0d
BrianScotty <scotty.brianleong@gmail.com>
2023-12-05 01:31:06 -0800

    fixed some bugs

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    x2_ist = my_list[:]
+    x2_list = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
             x2_list[count] = True
