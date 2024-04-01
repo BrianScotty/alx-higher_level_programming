0cbd688aebef718e12c6f40ac596e5cfdd6251f2
BrianScotty <scotty.brianleong@gmail.com>
2024-04-01 02:15:42 -0700

    corrections

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as resp:
+    with urllib.request.urlopen(request) as response:
         print(dict(resp.headers).get("X-Request-Id"))
--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,6 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
